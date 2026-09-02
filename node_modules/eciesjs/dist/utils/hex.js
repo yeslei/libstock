c5265500182e6426f9e9631802752fcd996c46d2
Seu Nome <danilodn993@gmail.com>
2026-09-02 00:54:50 -0300

    chore: remove node_modules do versionamento

--- node_modules/eciesjs/dist/utils/hex.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.decodeHex = exports.remove0x = void 0;
-const utils_1 = require("@noble/ciphers/utils");
-const remove0x = (hex) => hex.startsWith("0x") || hex.startsWith("0X") ? hex.slice(2) : hex;
-exports.remove0x = remove0x;
-const decodeHex = (hex) => (0, utils_1.hexToBytes)((0, exports.remove0x)(hex));
-exports.decodeHex = decodeHex;
--- node_modules/eciesjs/dist/utils/symmetric.d.ts
+++
@@ -1,3 +0,0 @@
-import type { Config } from "../config.js";
-export declare const symEncrypt: (config: Config, key: Uint8Array, plainText: Uint8Array, AAD?: Uint8Array) => Uint8Array;
-export declare const symDecrypt: (config: Config, key: Uint8Array, cipherText: Uint8Array, AAD?: Uint8Array) => Uint8Array;
